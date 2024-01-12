606a8d348ac809b5e2a39ede0d47563b4805bd0c
Raphaël Faure <raphael.faure2@student-cs.fr>
2024-01-12 16:23:36 +0100

    Clean main

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,4 @@
 // "Copyright 2024 Raphaël Faure"
 
-#include "log/logger.hpp"
-#include <iostream>
-#include <ostream>
-#include <valarray>
-
 int main(int argc, char *argv[]) {
-  Logger *logger = new Logger();
-  logger->info << "bb";
 }
-
-
